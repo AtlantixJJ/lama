0e1976a460f0da19431741ecb08b1369f1a050bd
Jianjin Xu <xujj15@gmail.com>
2022-11-22 16:31:44 -0800

    save

--- fetch_data/celebahq_id_gen_mask.py
+++ fetch_data/celebahq_id_gen_mask.py
@@ -1,15 +1,11 @@
 import os
 
-data_dir = "../../data/celebahq/export/"
+data_dir = "../../data/celebahq/id_inpaint/"
 dirs = os.listdir(data_dir)
 dirs.sort()
 for d in dirs:
     print(d)
     os.system(f"python3 bin/gen_mask_dataset.py \
         $(pwd)/configs/data_gen/random_thick_512.yaml \
-        ../../data/celebahq/export/{d}/train_image \
-        ../../data/celebahq/export/{d}/train_image_mask --ext png")
-    os.system(f"python3 bin/gen_mask_dataset.py \
-        $(pwd)/configs/data_gen/random_thick_512.yaml \
-        ../../data/celebahq/export/{d}/test_image \
-        ../../data/celebahq/export/{d}/test_image_mask --ext png")
+        ../../data/celebahq/id_inpaint/{d}/reference_image \
+        ../../data/celebahq/id_inpaint/{d}/reference_mask --ext png")
